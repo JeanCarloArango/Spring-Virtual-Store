1b6f6447faec2107b10ca627665753d8d5082482
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-10-08 09:20:58 -0500

    Finishing remote BD connection

--- src/main/java/com/tiendavirtual/dao/ConnectionDB.java
+++ src/main/java/com/tiendavirtual/dao/ConnectionDB.java
@@ -9,7 +9,7 @@ public class ConnectionDB {
 	private String password = "admin";
 	private String url = "jdbc:mysql://localhost/" + bd;*/
 	
-	private String bd = "Grupo02";
+	private String bd = "Grupo02BraveTeam";
 	private String login = "admin";
 	private String password = "MisionTIC2022GRUPO02";
 	private String url = "jdbc:mysql://misiontic2022grupo02.czo3ixoe3xoe.us-east-1.rds.amazonaws.com/" + bd;
@@ -36,7 +36,6 @@ public class ConnectionDB {
 			this.sentence = connection.prepareStatement(sql);
 			return this.sentence;
 		} catch (Exception e) {
-			// TODO: handle exception
 			return null;
 		}
 	}
