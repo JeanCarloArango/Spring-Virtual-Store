db8e88342ca6b6ba24d812e5c546489d8f32dd85
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-10-11 08:23:43 -0500

    Changing App name

--- src/main/java/com/uelbosque/tiendavirtual/TiendaVirtualSpringApplication.java
+++ src/main/java/com/uelbosque/tiendavirtual/TiendaVirtualSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TiendaVirtualSpringApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.servlet.context-path", "/TiendaVirtualApp");
+		System.setProperty("server.servlet.context-path", "/BraveTeamApp");
 		SpringApplication.run(TiendaVirtualSpringApplication.class, args);
 	}
 
