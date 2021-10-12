78be16d00f6db4762cc58570fcf0d7868fc27c6f
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-10-12 16:07:20 -0500

    Preparing for pulling

--- target/TiendaVirtualSpring-0.0.1-SNAPSHOT/script/scriptDesign.js
+++
@@ -1,9 +0,0 @@
-const links = document.querySelectorAll("#menu a");
-
-links.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        links.forEach((link) => link.classList.remove('active'));
-        e.target.classList.add('active');
-    });
-});
